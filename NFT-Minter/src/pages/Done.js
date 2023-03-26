afdc602dfa0a7a1283ffa93942cb4f7638fce415
이인수 <in63119@gmail.com>
2023-03-26 12:11:50 +0900

    Add Klip Logo

--- NFT-Minter/src/pages/Done.js
+++ NFT-Minter/src/pages/Done.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Image
-import afunLogo from "../img/afun_Logo.png";
+import kilpLogo from "../img/klip_Logo.png";
 
 // MUI css
 import { Box, Typography } from "@mui/material";
@@ -16,16 +16,8 @@ export default function Done() {
         mt: "10px",
       }}
     >
-      {/* <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          mt: "5%",
-        }}
-      > */}
       <Typography>NFT 민팅이 완료 되었습니다.</Typography>
       <Typography>잠시 후 Klip에서 확인해주세요.</Typography>
-      {/* </Box> */}
       <Box
         sx={{
           display: "flex",
@@ -33,7 +25,7 @@ export default function Done() {
           mt: "5%",
         }}
       >
-        <img src={afunLogo} width="200px" height="auto" alt="logo" />
+        <img src={kilpLogo} width="200px" height="auto" alt="logo" />
       </Box>
     </Box>
   );
