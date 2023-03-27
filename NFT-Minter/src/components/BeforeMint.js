31ad46df926ba45ae2d3bb32f4a2aeb613428b8b
이인수 <in63119@gmail.com>
2023-03-27 22:00:51 +0900

    Fix MetaData

--- NFT-Minter/src/components/BeforeMint.js
+++ NFT-Minter/src/components/BeforeMint.js
@@ -1,20 +1,27 @@
 import React, { useEffect } from "react";
 
 // MUI css
-import { Box, Card, CardContent, Typography, TextField } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  FormControl,
+  Input,
+  FormHelperText,
+} from "@mui/material";
 
 // recoil
 import { useRecoilValue, useSetRecoilState, useRecoilState } from "recoil";
 import { fileNumState } from "../recoil/files.js";
 import { nftMetaState } from "../recoil/nftMeta.js";
-import { networkState } from "../recoil/network.js";
-import { attributesState, attributeSelector } from "../recoil/attributes.js";
+import { attributeSelector } from "../recoil/attributes.js";
 
 export default function BeforeMint() {
   const fileNum = useRecoilValue(fileNumState);
+  const metaData = useRecoilValue(nftMetaState);
   const nftMeta = useSetRecoilState(nftMetaState);
-  const network = useRecoilValue(networkState);
-  const setPolygonAttributes = useSetRecoilState(attributesState);
   const [attributes, setAttributes] = useRecoilState(attributeSelector);
 
   const handleName = (e) => {
@@ -31,29 +38,29 @@ export default function BeforeMint() {
     }));
   };
 
-  const handleOwner = (e) => {
-    setPolygonAttributes([
-      {
-        trait_type: "Owner",
-        value: e.target.value,
-      },
-    ]);
-  };
+  // const handleOwner = (e) => {
+  //   setPolygonAttributes([
+  //     {
+  //       trait_type: "Owner",
+  //       value: e.target.value,
+  //     },
+  //   ]);
+  // };
 
-  const handleLeadPainter = (e) => {
+  // const handleLeadPainter = (e) => {
+  //   setAttributes((prev) => [
+  //     ...prev,
+  //     {
+  //       trait_type: "Artist",
+  //       value: e.target.value,
+  //     },
+  //   ]);
+  // };
+
+  const handleClass = (e) => {
     setAttributes((prev) => [
       ...prev,
-      {
-        trait_type: "Artist",
-        value: e.target.value,
-      },
-    ]);
-  };
-
-  const handleCollaboratedWith = (e) => {
-    setAttributes((prev) => [
-      ...prev,
-      { trait_type: "Collaborated with", value: e.target.value },
+      { trait_type: "Class", value: e.target.value },
     ]);
   };
 
@@ -63,6 +70,8 @@ export default function BeforeMint() {
       attributes: attributes,
     }));
   }, [attributes, nftMeta]);
+
+  console.log(metaData);
 
   const rendering = () => {
     const result = [];
@@ -93,38 +102,28 @@ export default function BeforeMint() {
                 fullWidth
                 onChange={handleDescription}
               />
-              {network.network === "Polygon" ? (
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="OwnerInput"
-                  label="Owner : NFT를 가질 소유자를 쓰세요."
-                  variant="standard"
-                  fullWidth
-                  onBlur={handleOwner}
+              <TextField
+                autoFocus
+                margin="dense"
+                id="CollaboratedWithInput"
+                label="Class : 아이는 무슨 반인가요?"
+                variant="standard"
+                fullWidth
+                onBlur={handleClass}
+              />
+              <FormControl
+                disabled
+                variant="standard"
+                margin="dense"
+                fullWidth
+                sx={{ mt: "23px" }}
+              >
+                <Input
+                  id="component-disabled"
+                  defaultValue="Kindergarten : 밀알 성품 어린이집"
                 />
-              ) : (
-                <Box>
-                  <TextField
-                    autoFocus
-                    margin="dense"
-                    id="ArtistInput"
-                    label="Artist : 작가의 이름을 쓰세요."
-                    variant="standard"
-                    fullWidth
-                    onBlur={handleLeadPainter}
-                  />
-                  <TextField
-                    autoFocus
-                    margin="dense"
-                    id="CollaboratedWithInput"
-                    label="Collaborated with : 함께한 아이를 쓰세요."
-                    variant="standard"
-                    fullWidth
-                    onBlur={handleCollaboratedWith}
-                  />
-                </Box>
-              )}
+                <FormHelperText>Only use 밀알 성품 어린이집</FormHelperText>
+              </FormControl>
             </CardContent>
           </Card>
         </Box>
--- NFT-Minter/src/recoil/attributes.js
+++ NFT-Minter/src/recoil/attributes.js
@@ -15,23 +15,25 @@ export const attributeSelector = selector({
     let filter = newValue.reverse();
     let result = [];
 
+    // for (let i = 0; i < filter.length; i++) {
+    //   if (filter[i].trait_type !== "" || filter[i].value !== "") {
+    //     if (filter[i].trait_type === "Artist") {
+    //       result.push(filter[i]);
+    //       break;
+    //     }
+    //   }
+    // }
+
     for (let i = 0; i < filter.length; i++) {
       if (filter[i].trait_type !== "" || filter[i].value !== "") {
-        if (filter[i].trait_type === "Artist") {
+        if (filter[i].trait_type === "Class") {
           result.push(filter[i]);
           break;
         }
       }
     }
 
-    for (let i = 0; i < filter.length; i++) {
-      if (filter[i].trait_type !== "" || filter[i].value !== "") {
-        if (filter[i].trait_type === "Collaborated with") {
-          result.push(filter[i]);
-          break;
-        }
-      }
-    }
+    result.push({ trait_type: "Kindergarten", value: "밀알 성품 어린이집" });
 
     set(attributesState, result);
   },
